088acfc88d1d968808da97b730b46d19c9a35839
DrW7F <89878175+DrW7F@users.noreply.github.com>
2022-05-01 17:29:25 -0300

    Update read-the-keyboard-and-send-bits.py

--- read-the-keyboard-and-send-bits.py
+++ read-the-keyboard-and-send-bits.py
@@ -1,6 +1,6 @@
 import serial, time
 from pynput.keyboard import Listener
-
+import pygame
 
 def key_recorder(key):
     key=str(key)
@@ -9,6 +9,8 @@ def key_recorder(key):
         time.sleep(2)
         dev.write(b'1')
         dev.close()
-
+        pygame.mixer.init()
+        pygame.mixer.music.load('soung.mp3')
+        pygame.mixer.music.play()
 with Listener(on_press=key_recorder) as l:
     l.join()
